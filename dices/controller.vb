3e91ba9c9273601e73794234bf9726df5e5445ae
falkonidas <falkonidas@wp.pl>
2015-03-05 13:52:23 +0100

    Delete controller.vb

--- dices/controller.vb
+++
@@ -1,6 +0,0 @@
-﻿Public MustInherit Class dicesRules
-    Protected dices As New List(Of dice)
-    Public Sub getDices(ByVal dices As List(Of dice))
-        Me.dices = dices
-    End Sub
-End Class
